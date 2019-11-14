fcf32b0e47972a585e38c301bf09de8674308bde
349310995 <349310995@402X1066HF0FFL2.EDU.YRDSB.NET>
2019-11-14 12:53:36 -0500

    Finished all images

--- Labyrinth/src/Assets.java
+++ Labyrinth/src/Assets.java
@@ -56,6 +56,15 @@ public class Assets {
 	public static ImageIcon tileDragon[] = new ImageIcon [4];
 	public static ImageIcon tileGhostWaving[] = new ImageIcon [4];
 	public static ImageIcon tileGhostBottle[] = new ImageIcon [4];
+	public static ImageIcon tileLadyPig[] = new ImageIcon [4];
+	public static ImageIcon tileLizard[] = new ImageIcon [4];
+	public static ImageIcon tileMoth[] = new ImageIcon [4];
+	public static ImageIcon tileSpider[] = new ImageIcon [4];
+	public static ImageIcon tileSorceress[] = new ImageIcon [4];
+	public static ImageIcon tileScarab[] = new ImageIcon [4];
+	public static ImageIcon tileRat[] = new ImageIcon [4];
+	public static ImageIcon tileOwl[] = new ImageIcon [4];
+	public static ImageIcon tileBat[] = new ImageIcon [4];
 	public static ImageIcon permenantTiles[] = new ImageIcon [16];
 	public static ImageIcon treasureCards[] = new ImageIcon [24];
 	//sddsb
@@ -88,15 +97,60 @@ public class Assets {
 		tileGhostBottle[2]= tileImageSetup("Images/GhostBottle2.png");
 		tileGhostBottle[3]= tileImageSetup("Images/GhostBottle3.png");
 		
-		treasureTileDragon[0] = tileImageSetup("Images/Dragon0.png");
-		treasureTileDragon[1] = tileImageSetup("Images/Dragon1.png");
-		treasureTileDragon[2] = tileImageSetup("Images/Dragon2.png");
-		treasureTileDragon[3] = tileImageSetup("Images/Dragon3.png");
-		
 		tileGhostWaving[0] = tileImageSetup("Images/GhostWaving0.png");
 		tileGhostWaving[1] = tileImageSetup("Images/GhostWaving1.png");
 		tileGhostWaving[2] = tileImageSetup("Images/GhostWaving2.png");
 		tileGhostWaving[3] = tileImageSetup("Images/GhostWaving3.png");
+		
+		tileLadyPig[0]= tileImageSetup("Images/LadyPig0.png");
+		tileLadyPig[1]= tileImageSetup("Images/LadyPig1.png");
+		tileLadyPig[2]= tileImageSetup("Images/LadyPig2.png");
+		tileLadyPig[3]= tileImageSetup("Images/LadyPig3.png");
+		
+		tileLizard[0]= tileImageSetup("Images/Lizard0.png");
+		tileLizard[1]= tileImageSetup("Images/Lizard1.png");
+		tileLizard[2]= tileImageSetup("Images/Lizard2.png");
+		tileLizard[3]= tileImageSetup("Images/Lizard3.png");
+		
+		tileMoth[0]= tileImageSetup("Images/Moth0.png");
+		tileMoth[1]= tileImageSetup("Images/Moth1.png");
+		tileMoth[2]= tileImageSetup("Images/Moth2.png");
+		tileMoth[3]= tileImageSetup("Images/Moth3.png");
+		
+		tileSpider[0]= tileImageSetup("Images/Spider0.png");
+		tileSpider[1]= tileImageSetup("Images/Spider1.png");
+		tileSpider[2]= tileImageSetup("Images/Spider2.png");
+		tileSpider[3]= tileImageSetup("Images/Spider3.png");
+		
+		tileSorceress[0]= tileImageSetup("Images/Sorceress0.png");
+		tileSorceress[1]= tileImageSetup("Images/Sorceress1.png");
+		tileSorceress[2]= tileImageSetup("Images/Sorceress2.png");
+		tileSorceress[3]= tileImageSetup("Images/Sorceress3.png");
+		
+		tileScarab[0]= tileImageSetup("Images/Scarab0.png");
+		tileScarab[1]= tileImageSetup("Images/Scarab1.png");
+		tileScarab[2]= tileImageSetup("Images/Scarab2.png");
+		tileScarab[3]= tileImageSetup("Images/Scarab3.png");
+		
+		tileRat[0]= tileImageSetup("Images/Rat0.png");
+		tileRat[1]= tileImageSetup("Images/Rat1.png");
+		tileRat[2]= tileImageSetup("Images/Rat2.png");
+		tileRat[3]= tileImageSetup("Images/Rat3.png");
+		
+		tileOwl[0]= tileImageSetup("Images/Owl0.png");
+		tileOwl[1]= tileImageSetup("Images/Owl1.png");
+		tileOwl[2]= tileImageSetup("Images/Owl2.png");
+		tileOwl[3]= tileImageSetup("Images/Owl3.png");
+		
+		tileBat[0]= tileImageSetup("Images/Bat0.png");
+		tileBat[1]= tileImageSetup("Images/Bat1.png");
+		tileBat[2]= tileImageSetup("Images/Bat2.png");
+		tileBat[3]= tileImageSetup("Images/Bat3.png");
+		
+		treasureTileDragon[0] = tileImageSetup("Images/Dragon0.png");
+		treasureTileDragon[1] = tileImageSetup("Images/Dragon1.png");
+		treasureTileDragon[2] = tileImageSetup("Images/Dragon2.png");
+		treasureTileDragon[3] = tileImageSetup("Images/Dragon3.png");
 		
 		permenantTiles[0] = tileImageSetup("Images/RedCornerTile.png");
 		permenantTiles[1] = tileImageSetup("Images/BookTile.png");
